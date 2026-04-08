314acd16e2b6debc7727a262fb1c7d97c3e51d9d
wogus5695-ctrl <wogus5695@gmail.com>
2026-04-08 16:56:50 +0900

    style: replace hero image with actual site photo

--- src/components/sections/LocalHero.tsx
+++ src/components/sections/LocalHero.tsx
@@ -95,7 +95,7 @@ export default function LocalHero({ locationName, serviceTitle, title, intro, ke
           <div className="relative order-1 lg:order-2">
             <div className="relative rounded-[40px] md:rounded-[60px] overflow-hidden shadow-2xl z-20 aspect-[4/3] md:aspect-square lg:aspect-[4/5]">
               <Image 
-                src="/hero-caulking.png"
+                src="/hero-caulking.jpg"
                 alt={`${locationName} 창틀코킹 시공 현장`}
                 fill
                 className="object-cover"
